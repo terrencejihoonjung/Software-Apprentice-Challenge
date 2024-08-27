fc6a97640027a71a1e2a62a6f3a1b13bbe9dca7a
terrencejihoonjung <terrencejung928@gmail.com>
2024-08-27 15:45:24 -0700

    Add: Created UI Skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen">
+      {/* Container */}
+      <div className="h-full mx-40 py-20 border">
+        {/* Header */}
+        <div className="w-full flex flex-col space-y-4 mb-4">
+          {/* Title */}
+          <h1 className="text-3xl font-bold">Blueprint Ads Analytics</h1>
+
+          {/* Search + Filter/Sort */}
+          <div className="w-full flex items-center space-x-3">
+            <input
+              placeholder="Enter campaign name here"
+              className="p-3 border w-1/4 rounded-lg"
+            />
+
+            <button className="text-md font-semibold border px-4 p-3 rounded-md">
+              Sort by Spend
+            </button>
+
+            <div className="flex items-center">
+              <button className="text-md font-semibold border px-4 py-3 rounded-md rounded-r-none">
+                Asc
+              </button>
+              <button className="text-md font-semibold border px-4 p-3 rounded-md rounded-l-none">
+                Desc
+              </button>
+            </div>
+
+            <button className="text-md font-semibold border px-4 p-3 rounded-md">
+              Reset
+            </button>
+          </div>
+        </div>
+
+        {/* Cards Container */}
+        <div className="w-full grid grid-cols-4 gap-4">
+          <div className="border h-72 rounded-md">hi</div>
+          <div className="border h-72 rounded-md">hi</div>
+          <div className="border h-72 rounded-md">hi</div>
+          <div className="border h-72 rounded-md">hi</div>
+          <div className="border h-72 rounded-md">hi</div>
+          <div className="border h-72 rounded-md">hi</div>
+        </div>
+      </div>
     </div>
   );
 }
